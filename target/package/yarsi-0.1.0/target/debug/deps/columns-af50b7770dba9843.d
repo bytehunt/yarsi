e17432b114002eac929696a35a3aa493e618b99b
pwnwriter <imnabeen01@gmai.com>
2023-01-25 09:56:56 +0545

    removing unnecessary keywords

---
+++ target/package/yarsi-0.1.0/target/debug/deps/columns-af50b7770dba9843.d
@@ -0,0 +1,7 @@
+/home/pwn/yarsi/target/package/yarsi-0.1.0/target/debug/deps/columns-af50b7770dba9843.rmeta: /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/columns-0.1.0/src/lib.rs
+
+/home/pwn/yarsi/target/package/yarsi-0.1.0/target/debug/deps/libcolumns-af50b7770dba9843.rlib: /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/columns-0.1.0/src/lib.rs
+
+/home/pwn/yarsi/target/package/yarsi-0.1.0/target/debug/deps/columns-af50b7770dba9843.d: /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/columns-0.1.0/src/lib.rs
+
+/home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/columns-0.1.0/src/lib.rs:
---
+++ target/package/yarsi-0.1.0/target/debug/deps/whoami-537b8fdd12e48787.d
@@ -0,0 +1,8 @@
+/home/pwn/yarsi/target/package/yarsi-0.1.0/target/debug/deps/whoami-537b8fdd12e48787.rmeta: /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/whoami-1.3.0/src/lib.rs /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/whoami-1.3.0/src/unix.rs
+
+/home/pwn/yarsi/target/package/yarsi-0.1.0/target/debug/deps/libwhoami-537b8fdd12e48787.rlib: /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/whoami-1.3.0/src/lib.rs /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/whoami-1.3.0/src/unix.rs
+
+/home/pwn/yarsi/target/package/yarsi-0.1.0/target/debug/deps/whoami-537b8fdd12e48787.d: /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/whoami-1.3.0/src/lib.rs /home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/whoami-1.3.0/src/unix.rs
+
+/home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/whoami-1.3.0/src/lib.rs:
+/home/pwn/.local/share/cargo/registry/src/github.com-1ecc6299db9ec823/whoami-1.3.0/src/unix.rs:
